b0827fb1fd75a98ef17829e5355111cd5cd1d9f5
Akshoy <akshoygorai33@gmail.com>
2024-10-23 17:26:04 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,13 @@ require('dotenv').config();
 const cors = require('cors');
 //hi
 const app = express();
-const corsOptions = {
-    origin: 'https://test-sigma-eosin-14.vercel.app/', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials:true,
-    // allowedHeaders: ['Content-Type','Authorization','multipart/form-data'],
-    // optionsSuccessStatus: 200, 
-  };
+// const corsOptions = {
+//     origin: 'https://test-sigma-eosin-14.vercel.app/', 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials:true,
+//     // allowedHeaders: ['Content-Type','Authorization','multipart/form-data'],
+//     // optionsSuccessStatus: 200, 
+//   };
   
 app.use(cors(corsOptions));
 app.use(morgan('dev'));
