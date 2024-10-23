a4df0b9e14f26a89b3ceb82fdc5419b22a0ab8b5
Akshoy <akshoygorai33@gmail.com>
2024-10-23 16:41:52 +0530

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://test-gjk7.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type','Authorization','multipart/form-data'],
     optionsSuccessStatus: 200, 
